b5b56d4629271f22f2e5846adf5296439f0d05d7
David A. Kennedy <me@davidakennedy.com>
2016-07-06 12:55:11 -0400

    Add `h1` to index page if it's not front page.
    * Improves accessibility and heading structure of theme.

--- index.php
+++ index.php
@@ -16,9 +16,17 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php if ( have_posts() ) : ?>
+		<?php if ( have_posts() ) :
 
-			<?php /* Start the Loop */ ?>
+			if ( is_home() && ! is_front_page() ) : ?>
+				<header>
+					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+				</header>
+
+			<?php
+			endif;
+
+			/* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php
