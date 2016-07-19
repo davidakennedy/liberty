7a39a6990dc59394cbdc90ee9988b19d36c86dbc
David A. Kennedy <me@davidakennedy.com>
2016-07-19 09:07:20 -0400

    Fix fonts and script version numbers.
    * Fonts in editor stylesheet are now correct.
    * Script version numbers match theme for consistency.

--- functions.php
+++ functions.php
@@ -166,14 +166,14 @@ function liberty_scripts() {
 
 	wp_enqueue_style( 'liberty-fonts', liberty_fonts_url(), array(), null );
 
-	wp_enqueue_script( 'liberty-script', get_template_directory_uri() . '/js/main.js', array(), '20150623', true );
+	wp_enqueue_script( 'liberty-script', get_template_directory_uri() . '/js/main.js', array(), '1.0', true );
 
 	$adminbar = is_admin_bar_showing();
 	wp_localize_script( 'liberty-script', 'libertyadminbar', array( $adminbar ) );
 
-	wp_enqueue_script( 'liberty-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+	wp_enqueue_script( 'liberty-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '1.0', true );
 
-	wp_enqueue_script( 'liberty-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	wp_enqueue_script( 'liberty-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '1.0', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
